50f0c050556dbaaff5778e28871cbd9952c2d509
Simon Lemire <simon.lemire@mandiant.com>
2025-08-01 10:09:40 -0400

    Add url blacklist/whitelist

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var urlBlacklist = parseEnvList(process.env.CORSANYWHERE_URL_BLACKLIST);
+var urlWhitelist = parseEnvList(process.env.CORSANYWHERE_URL_WHITELIST);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -23,6 +25,8 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
+  urlBlacklist: urlBlacklist,
+  urlWhitelist: urlWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
